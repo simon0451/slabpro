6eb0b6d81da85ed74f1019ed031ea0cbfaf9d941
Simon <sapopecki@gmail.com>
2017-12-06 21:17:12 -0500

    fixed directory problem on code

--- PID_control_new/PID_control_new.ino
+++ PID_control_new/PID_control_new.ino
@@ -13,111 +13,114 @@
 
 #include <Servo.h> //required for controlling an ESC
 
-// Define Constants
+//Constants
 const uint8_t DRIVE = 2; //PWM out on pin 2
 const uint8_t ECHO = 7; //The echo pin on the HC-SR04 ultrasonic sensor
 const uint8_t TRIG = 8; //The trigger pin on the HC-SR04 ultrasonic sensor
 const uint16_t MAXDISTANCE = 130; //This makes it so the sensor won't try to resolve distances outside the sensor's range
 const uint16_t MINDISTANCE = 0; //This sets the minimum range of the sensor to 0 for obvious reasons
 const uint8_t STOPDISTANCE = 10; //This sets the stopping distance
-const uint8_t KP = 1; // Proportional gain tuning parameter
-const uint8_t KI = 1; // Integral gain tuning parameter
-const uint8_t KD = 1; // Derivative gain tuning parameter
-const uint8_t ITER_TIME = 1; // Record every 10 ms
-const uint8_t MOTOR_MAX_FORWARD = 100; // Motor forward max speed
-const uint8_t MOTOR_MAX_BACKWARD = 0; // Motor backward max speed
-const uint8_t MOTOR_STOP = 50; // Motor stop value
-const uint16_t BAUD = 9600; //Serial communications rate for debugging
-
-// Initialize Variables
+const uint8_t KP = 1; // Proportional gain term
+const uint8_t KI = 1; // Integral gain term
+const uint8_t KD = 1; // Derivative gain term
+const uint8_t ITER_TIME = 10; // Record every 10 ms
+//Variables
 uint16_t duration = 0; //Used to calculate distance
 uint16_t distance = 130; //cm, Distance to obstacle, starts at 255 because the ultrasonic sensor won't return a value larger than this
-uint16_t throttle = 50; //The speed of the robot as understood by the Sabertooth 2x12 RC ESC
-uint8_t error = 0; //Initial error
-uint8_t distancein = 0;
+uint8_t throttle = 50; //The speed of the robot as understood by the Sabertooth 2x12 RC ESC
+uint8_t error_init = 0; //Initial error
 
 Servo SABERTOOTH; //Creating a servo object to represent forward/rearward motion input to the ESC
 
 uint8_t FindDistance() //This function uses the HC-SR04 ultrasonic sensor to find the distance to the wall 
 {
-	digitalWrite(TRIG, LOW);
-	delayMicroseconds(2);
-	digitalWrite(TRIG, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(TRIG, LOW);
-	duration = pulseIn(ECHO, HIGH); //Finding the time between the
-	distance = duration/58.2; //This will truncate the value so that it's an integer
-	if (distance >= MAXDISTANCE) //If the distance is out of range or larger than the MAXDISTANCE value, the sensor will just assume it is the MAXDISTANCE
-	{
-		return MAXDISTANCE;
-	}
-	if (distance <= MINDISTANCE) //Likewise for the MINDISTANCE
-	{
-		return 0;
-	}
-	else
-	{
-		return distance; //The function returns the distance in cm
-	}
-}
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, LOW);
+  duration = pulseIn(ECHO, HIGH); //Finding the time between the
+  distance = duration/58.2; //This will truncate the value so that it's an integer
 
-uint16_t calcPID(uint8_t error_in, uint8_t distancein) // add int_in if start condition is not zero
-{
-	uint8_t error_prior = error_in;
-	uint8_t error = distance - STOPDISTANCE; // PID sensor distance - target distance
-	uint16_t proportional = KP*error;
-	uint16_t integral = KI*error*ITER_TIME;
-	uint16_t derivative = KD*(error - error_prior)/ITER_TIME;
-	uint16_t outputPID = proportional + integral + derivative;
-	// limit the motor speed
-	if (outputPID > MOTOR_MAX_FORWARD)
-	{
-		outputPID = MOTOR_MAX_FORWARD;
-	}
-	if (outputPID < MOTOR_MAX_BACKWARD)
-	{
-		outputPID = MOTOR_MAX_BACKWARD;
-	}
-	
-	if (error = 0)
-	{
-		outputPID = MOTOR_STOP; // stop
-		return outputPID;
-	}
-	
-	return outputPID;
+  if (distance >= MAXDISTANCE) //If the distance is out of range or larger than the MAXDISTANCE value, the sensor will just assume it is the MAXDISTANCE
+  {
+    return MAXDISTANCE;
+  }
+  if (distance <= MINDISTANCE) //Likewise for the MINDISTANCE
+  {
+    return 0;
+  }
+  else
+  {
+    return distance; //The function returns the distance in cm
+  }
 }
-
 
 void setup()
 {
-  Serial.begin(BAUD);
-	pinMode(TRIG, OUTPUT); //Initializing the sonar control pins
-	pinMode(ECHO, INPUT);
-	
-	SABERTOOTH.attach(DRIVE); //the servo object is bound to pin 2 (DRIVE) and SABERTOOTH is used to refer to the ESC from here on out
-	SABERTOOTH.writeMicroseconds(1500); //Initializing ESC at zero throttle
-  delay(500);
+  pinMode(TRIG, OUTPUT); //Initializing the sonar control pins
+  pinMode(ECHO, OUTPUT);
+  
+  SABERTOOTH.attach(DRIVE); //the servo object is bound to pin 2 (DRIVE) and SABERTOOTH is used to refer to the ESC from here on out
+  SABERTOOTH.writeMicroseconds(1500); //Initializing ESC at zero throttle
 }
 
+int calcPID(error_in,distanceToObstacle,distance,iteration_time) // add int_in if start condition is not zero
+{
+	uint8_t error_prior = error_in;
+	// integral = int_in;
+	// uint8_t bias = 0; 			// offset error parameter, left zero for now
+	// KP = kp_in; 			// must be a predetermined constant
+	// KI = ki_in; 			// must be a predetermined constant
+	// KD = kd_in; 			// must be a predetermined constant
+	// iteration_time = 10; 		// 10 ms wait time between iterations
+	motor_max = 100;		// max forward speed
+	motor_min = 0; 			// max backwards speed
+	motor_stop = 50;		// stops the motor
+	
+	// Calculate the error
+	error = distance - distanceToObstacle; // PID sensor distance - target distance
+
+	// Calculate the integral
+	// integral = integral + (error*iteration_time);
+	integral = error*iteration_time;
+
+	// Calculate the derivative
+	derivative = (error - error_prior)/iteration_time;
+	
+	// Calculate the control variable
+	outputPID = (KP*error) + (KI*integral) + (KD*derivative); // + bias;
+
+	// limit the motor speed
+	if (outputPID > motor_max)
+	{
+		outputPID = motor_max;
+	}
+	if (outputPID < motor_min)
+	{
+		outputPID = motor_min;
+	}
+
+	if (error = 0)
+	{
+		outputPID = motor_stop; // stop
+		return outputPID;
+	}
+
+	return outputPID;
+}
 
 void loop()
 {
 	distance = FindDistance();
-  Serial.print("Distance: ");
-  Serial.println(distance);
+	
 	//Do some PID shit here
-	throttle = calcPID(error,distance);
-  Serial.print("throttle pre math: ");
-  Serial.println(throttle);
-	error = distance - STOPDISTANCE;
-  Serial.print("error: ");
-  Serial.println(error);
+	PID_out = calcPID(error_init,KP,KI,KD,STOPDISTANCE,distance,ITER_TIME);
+	error_init = distance - STOPDISTANCE;
+	
 	throttle = (throttle*10)+1000; //Converts the throttle value (0-50-100) to a pulsewidth in microseconds understandable by the MEGA
-  Serial.print("throttle: ");
-  Serial.println(throttle);
 	SABERTOOTH.writeMicroseconds(throttle); //Sending the command to the ESC
 	
 	// wait for next iteration of function
-	delay(ITER_TIME);
+	sleep(ITER_TIME);
 }
